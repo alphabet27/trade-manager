d27636bef002b9955806350ba47ff7f518b15e1a
alphabet27 <104099987+alphabet27@users.noreply.github.com>
2024-03-08 10:17:32 +0530

    Delete scripts/read_addnl.py

--- scripts/read_addnl.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def var_reader(df):    
-    with open('sample_database/addnl_stock.csv', 'r') as f:
-        for line in f:
-            df = pd.concat( [df, pd.DataFrame([tuple(line.strip().split(','))])], ignore_index=True )
-
-
-
